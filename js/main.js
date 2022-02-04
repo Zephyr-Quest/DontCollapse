530546a691e1a52fab3345d7c0704385cedfbe81
EnguerrandMQT <enguerrand.marquant@student.junia.com>
2022-02-04 13:53:07 +0100

    Un shop qui alterne entre machine, ressources et personnel dans une modale, et bim

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
-let id = 1;
-$().on('scroll', function(e) {
-    //e.preventDefault();
-    id += 20;
-    console.log(id);
+let id = 200;
+let stat = true;
+const update = () => {
+    if (stat) id += 1;
+    else id -= 1;
 
+    if (id > 370 || id < 10) stat = !stat;
     $('.progress').width(id);
-    if (id >= 400) {
-        id = 1
-    }
+
+    setTimeout(update, 15);
+}
+update();
+
+
+
+
+
+// Get the modal
+var modal = $('#modal1');
+
+// When the user clicks on the button, open the modal
+$('#shop').on('click', function () {
+    modal.css('display', 'flex');
 });
 
+// When the user clicks on <span> (x), close the modal
+$('.close').on('click', function () {
+    modal.css('display', 'none');
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == document.getElementById("modal1")) {
+        modal.css('display', 'none');
+    }
+}
