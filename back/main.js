eb9e31b042a4076ddd14919661dbe49c6202c843
Rémi VAN BOXEM <remi.van-boxem@student.junia.com>
2022-05-16 09:18:27 +0200

    correct

--- back/main.js
+++ back/main.js
@@ -8,10 +8,13 @@ party.addPlayer("Xavier");
 let Damien = party.searchPlayer("Damien");
 
 // Damien.machineDisplay();
-Damien.money = 10000000;
-Damien.machineUpgrade(0,4);
-Damien.machineUpgrade(1,4);
-Damien.machineUpgrade(2,4);
-Damien.machineUpgrade(3,4);
+
+Damien.updateAll();
+Damien.updateAll();
+Damien.updateAll();
+Damien.updateAll();
+// Damien.recruteEmployee("engineers");
+// // Damien.recruteEmployee("maintainers");
+Damien.machineUpgrade(0,2);
 Damien.updateAll();
 Damien.financesDisplay();
