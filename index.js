9323fc395947e230fef6f698a8a598ba6ec77854
Enguerrand Marquant <enguerrand.marquant@student.yncrea.fr>
2022-05-17 15:30:34 +0200

    push pour merge

--- index.js
+++ index.js
@@ -82,7 +82,6 @@ function getMaxKey(obj) {
 const updateMonth = game => {
     const players = io.sockets.adapter.rooms.get(game.idRoom);
     const event = game.applyEvent();
-    game.event = event;
 
     for (const p of players) {
         const pSocket = io.sockets.sockets.get(p);
@@ -118,7 +117,6 @@ const updateMonth = game => {
             }
         }
     }
-    game.event = undefined;
 };
 
 const endGame = game => {
